b66aaec471d3961e59ed2e7709007823144a60f0
Daniel Berndt <daniel.berndt@gmail.com>
2018-10-17 13:09:18 +0200

    docs: fix order

--- doczrc.js
+++ doczrc.js
@@ -5,7 +5,13 @@ export default {
   title: "React Sticky Box",
   description: "Sticky scrolling for all contents of all sizes",
   src: "docs",
-  ordering: "ascending",
+  menu: [
+    "React Sticky Box",
+    "Api",
+    {name: "Examples", menu: ["Simple", "Complex", "Full Page"]},
+    "Changelog",
+  ],
+
   modifyBundlerConfig: config => {
     config.optimization.splitChunks = {
       automaticNameDelimiter: "-",
