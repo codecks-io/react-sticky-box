56e2c24750f14448a1770f7854f4f1fd2027e2dd
Daniel Berndt <daniel.berndt@gmail.com>
2018-12-02 16:34:43 +0100

    escaping globs since zsh !== bash, it seems

--- doczrc.js
+++ doczrc.js
@@ -5,6 +5,9 @@ export default {
   title: "React Sticky Box",
   description: "Sticky scrolling for all contents of all sizes",
   src: "docs",
+  themeConfig: {
+    showPlaygroundEditor: true,
+  },
   menu: [
     "React Sticky Box",
     "Api",
