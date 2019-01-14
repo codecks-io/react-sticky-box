b23e31451e4af1864d9e30060d7adf82b94fc9a3
Daniel Berndt <daniel.berndt@gmail.com>
2019-01-14 14:39:58 +0100

    make typescript definitions more specific

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,20 @@
 import * as React from "react";
 import ResizeObserver from "resize-observer-polyfill";
 
-interface StickyBoxProps
-  extends React.HTMLAttributes<HTMLElement> {
-  offset?: number;
+export type StickyBoxMode = "relative" | "stickyBottom" | "stickyTop";
+
+interface StickyBoxProps {
   offsetTop?: number;
   offsetBottom?: number;
   bottom?: boolean;
-  onChangeMode?: () => void;
+  onChangeMode?: (oldMode: StickyBoxMode | undefined, newMode: StickyBoxMode) => any;
+  style?: React.CSSProperties;
+  className?: string;
+  children: React.ReactNode;
 }
 
 export interface StickyBoxInstance {
-  mode: string;
+  mode: StickyBoxMode;
   node: HTMLElement;
   scrollPane: HTMLElement | Window;
   parentHeight: number;
@@ -19,11 +22,8 @@ export interface StickyBoxInstance {
   nodeHeight: number;
   viewportHeight: number;
   offset: number;
-  updateNode: () => void;
-  switchToStickyBottom: () => void;
-  ron: ResizeObserver;
-  ropn: ResizeObserver;
-};
+  latestScrollY: number;
+}
 
 declare const IStickyBox: React.ComponentClass<StickyBoxProps>;
 
