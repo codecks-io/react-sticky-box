d02acc4bca05df820f9e9f7e3d043ed7252ff16a
Daniel Berndt <daniel.berndt@gmail.com>
2020-02-27 15:45:13 +0100

    update dev dependencies

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,3 @@
-import path from "path";
-import {css} from "docz-plugin-css";
-
 export default {
   title: "React Sticky Box",
   description: "Sticky scrolling for all contents of all sizes",
@@ -13,27 +10,5 @@ export default {
     "Api",
     {name: "Examples", menu: ["Simple", "Complex", "Full Page", "Debug"]},
     "Changelog",
-  ],
-
-  modifyBundlerConfig: config => {
-    config.optimization.splitChunks = {
-      automaticNameDelimiter: "-",
-      ...config.optimization.splitChunks,
-    };
-    config.optimization.runtimeChunk = {
-      ...config.optimization.runtimeChunk,
-      name: entrypoint => `runtime-${entrypoint.name}`,
-    };
-    config.resolve.alias = {
-      "react-sticky-box": path.join(__dirname, "src"),
-      ...config.resolve.alias,
-    };
-    return config;
-  },
-  plugins: [
-    css({
-      preprocessor: "postcss",
-      cssmodules: true,
-    }),
   ],
 };
---
+++ gatsby-node.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+exports.onCreateWebpackConfig = args => {
+  args.actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        'react-sticky-box': path.resolve(__dirname, '../src/index'),
+      },
+    },
+  })
+}
